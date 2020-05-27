4698f6b6b4c9594e1e1c95c4d2af33182d47e604
Prateek Gurnani <prateekgurnani10@gmail.com>
2020-05-27 06:45:33 -0400

    🚀 RELEASE: deplyed the blog

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="author">
 
-		<g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
+		<g-image alt="Author image" class="author__image" src="~/assets/images/mypic.jpeg" width="180" height="180" blur="5" />
 
 		<h1 v-if="showTitle" class="author__site-title">
 			{{ $static.metadata.siteName }}
@@ -12,8 +12,8 @@
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+			<a href="//twitter.com/Prateek_theDev">Follow on Twitter</a>
+			<a href="//github.com/prateekgurnani10">GitHub</a>
 		</p>
 
 	</div>
